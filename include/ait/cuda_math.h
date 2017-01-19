d4243ac001a78da6c525d3695b9364a1af4b554c
Benjamin Hepp <benjamin.hepp@posteo.de>
2017-01-19 13:52:23 +0100

    Added CUDA support headers

---
+++ include/ait/cuda_math.h
@@ -0,0 +1,349 @@
+//==================================================
+// cuda_math.h
+//
+//  Copyright (c) 2017 Benjamin Hepp.
+//  Author: Benjamin Hepp
+//  Created on: Jan 17, 2017
+//==================================================
+
+#pragma once
+
+#include <ait/cuda_utils.h>
+#include <cstdint>
+#include <cfloat>
+
+template <typename FloatT, std::size_t _Rows>
+class CudaVector {
+public:
+  using FloatType = FloatT;
+  static const std::size_t Rows = _Rows;
+
+  __host__ __device__
+  static CudaVector Zero() {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = 0;
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  std::size_t size() const {
+    return Rows;
+  }
+
+  __host__ __device__
+  const FloatT* data() const {
+    return vector_;
+  }
+
+  __host__ __device__
+  FloatT* data() {
+    return vector_;
+  }
+
+  __host__ __device__
+  const FloatT& operator()(const std::size_t index) const {
+    return vector_[index];
+  }
+
+  __host__ __device__
+  FloatT& operator()(const std::size_t index) {
+    return vector_[index];
+  }
+
+  __host__ __device__
+  void copyFrom(const FloatT* data) {
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vector_[i] = data[i];
+    }
+  }
+
+  __host__ __device__
+  void copyTo(FloatT* data) const {
+    for (std::size_t i = 0; i < Rows; ++i) {
+      data[i] = vector_[i];
+    }
+  }
+
+  __host__ __device__
+  CudaVector cwiseMin(const CudaVector& other) const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = std::min(vector_[i], other(i));
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector cwiseMax(const CudaVector& other) const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = std::max(vector_[i], other(i));
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector cwiseInverse() const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = (FloatT)1 / vector_[i];
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector operator+(const CudaVector& other) const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = vector_[i] + other(i);
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector operator-(const CudaVector& other) const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = vector_[i] - other(i);
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector operator*(const FloatT& scalar) const {
+    CudaVector vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = vector_[i] * scalar;
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  FloatT squaredNorm() const {
+    FloatT squared_norm = 0;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      squared_norm += vector_[i] * vector_[i];
+    }
+    return squared_norm;
+  }
+
+  __host__ __device__
+  void print() const {
+    for (std::size_t row = 0; row < Rows; ++row) {
+      if (row == 0) {
+        printf("%f", (*this)(row));
+      }
+      else {
+        printf(" %f", (*this)(row));
+      }
+    }
+    printf("\n");
+  }
+
+private:
+  FloatT vector_[Rows];
+};
+
+template <typename FloatT, std::size_t _Rows, std::size_t _Cols>
+class CudaMatrix {
+public:
+  enum StorageOrder {
+    RowMajor,
+    ColumnMajor,
+  };
+
+  using FloatType = FloatT;
+  static const std::size_t Rows = _Rows;
+  static const std::size_t Cols = _Cols;
+
+  __host__ __device__
+  static CudaMatrix Zero() {
+    CudaMatrix mat;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      for (std::size_t j = 0; j < Cols; ++j) {
+        mat(i, j) = 0;
+      }
+    }
+    return mat;
+  }
+
+  __host__ __device__
+  std::size_t size() const {
+    return Rows * Cols;
+  }
+
+  __host__ __device__
+  const FloatT* data() const {
+    return matrix_;
+  }
+
+  __host__ __device__
+  FloatT* data() {
+    return matrix_;
+  }
+
+  __host__ __device__
+  CudaVector<FloatT, Rows> col(const std::size_t col) const {
+    CudaVector<FloatT, Rows> vec;
+    for (std::size_t row = 0; row < Rows; ++row) {
+      vec(row) = (*this)(row, col);
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaVector<FloatT, Cols> row(const std::size_t row) const {
+    CudaVector<FloatT, Cols> vec;
+    for (std::size_t col = 0; col < Cols; ++col) {
+      vec(col) = (*this)(row, col);
+    }
+    return vec;
+  }
+
+  template <std::size_t row_offset, std::size_t col_offset, std::size_t row_length, std::size_t col_length>
+  __host__ __device__
+  CudaMatrix<FloatT, row_length, col_length> block() const {
+    CudaMatrix<FloatT, row_length, col_length> block_matrix;
+//    static_assert(row_offset + row_length <= Rows);
+//    static_assert(col_offset + col_length <= Cols);
+    for (std::size_t row = row_offset; row < row_offset + row_length; ++row) {
+      for (std::size_t col = col_offset; col < col_offset + col_length; ++col) {
+        block_matrix(row - row_offset, col - col_offset) = (*this)(row, col);
+      }
+    }
+    return block_matrix;
+  }
+
+  __host__ __device__
+  CudaVector<FloatT, Rows> operator*(const CudaVector<FloatT, Cols>& vec) {
+    CudaVector<FloatT, Rows> result_vec = CudaVector<FloatT, Rows>::Zero();
+    for (std::size_t row = 0; row < Rows; ++row) {
+      for (std::size_t col = 0; col < Cols; ++col) {
+        result_vec(row) += (*this)(row, col) * vec(col);
+      }
+    }
+    return result_vec;
+  }
+
+  __host__ __device__
+  const FloatT& operator()(const std::size_t index) const {
+    return matrix_[index];
+  }
+
+  __host__ __device__
+  FloatT& operator()(const std::size_t index) {
+    return matrix_[index];
+  }
+
+  __host__ __device__
+  const FloatT& operator()(const std::size_t row, const std::size_t col) const {
+    return matrix_[row * Cols + col];
+  }
+
+  __host__ __device__
+  FloatT& operator()(const std::size_t row, const std::size_t col) {
+    return matrix_[row * Cols + col];
+  }
+
+  __host__ __device__
+  void copyFrom(const FloatT* data, const StorageOrder storage_order = RowMajor) {
+    if (storage_order == RowMajor) {
+      for (std::size_t i = 0; i < size(); ++i) {
+        matrix_[i] = data[i];
+      }
+    }
+    else if (storage_order == ColumnMajor) {
+      for (std::size_t row = 0; row < Rows; ++row) {
+        for (std::size_t col = 0; col < Cols; ++col) {
+          (*this)(row, col) = data[col * Rows + row];
+        }
+      }
+    }
+  }
+
+  __host__ __device__
+  void copyTo(FloatT* data, const StorageOrder storage_order = RowMajor) const {
+    if (storage_order == RowMajor) {
+      for (std::size_t i = 0; i < size(); ++i) {
+        data[i] = (*this)(i);
+      }
+    }
+    else if (storage_order == ColumnMajor) {
+      for (std::size_t row = 0; row < Rows; ++row) {
+        for (std::size_t col = 0; col < Cols; ++col) {
+          data[col * Rows + row] = (*this)(row, col);
+        }
+      }
+    }
+  }
+
+  __host__ __device__
+  CudaMatrix cwiseMin(const CudaMatrix& other) const {
+    CudaMatrix mat;
+    for (std::size_t i = 0; i < size(); ++i) {
+      mat(i) = std::min(matrix_[i], other(i));
+    }
+    return mat;
+  }
+
+  __host__ __device__
+  CudaMatrix cwiseMax(const CudaMatrix& other) const {
+    CudaMatrix vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = std::max(matrix_[i], other(i));
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaMatrix cwiseInverse() const {
+    CudaMatrix vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = (FloatT)1 / matrix_[i];
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaMatrix operator+(const CudaMatrix& other) const {
+    CudaMatrix vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = matrix_[i] + other(i);
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaMatrix operator-(const CudaMatrix& other) const {
+    CudaMatrix vec;
+    for (std::size_t i = 0; i < Rows; ++i) {
+      vec(i) = matrix_[i] - other(i);
+    }
+    return vec;
+  }
+
+  __host__ __device__
+  CudaMatrix operator*(const FloatT& scalar) const {
+    CudaMatrix mat;
+    for (std::size_t i = 0; i < size(); ++i) {
+      mat(i) = matrix_[i] * scalar;
+    }
+    return mat;
+  }
+
+  __host__ __device__
+  void print() const {
+    for (std::size_t row = 0; row < Rows; ++row) {
+      for (std::size_t col = 0; col < Cols; ++col) {
+        printf("%f ", (*this)(row, col));
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+
+private:
+  FloatT matrix_[Rows * Cols];
+};
---
+++ include/ait/cuda_utils.h
@@ -0,0 +1,190 @@
+//==================================================
+// cuda.h
+//
+//  Copyright (c) 2017 Benjamin Hepp.
+//  Author: Benjamin Hepp
+//  Created on: Jan 16, 2017
+//==================================================
+#pragma once
+
+#include <string>
+
+#include <ait/common.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#ifdef __CDT_PARSER__
+  #define __global__
+  #define __device__
+  #define __shared__
+  #define __host__
+#endif
+
+#define CUDA_DEVICE_SYNCHRONIZE()   CUDA_SAFE_CALL(cudaDeviceSynchronize());
+
+#define CUDA_SAFE_CALL(call) { \
+    cudaError err = call; \
+    if (cudaSuccess != err) { \
+      fprintf(stderr, "CUDA error in file '%s' in line %i: %s\n", \
+          __FILE__, __LINE__, cudaGetErrorString(err)); \
+      AIT_DEBUG_BREAK; \
+      AIT_EXCEPTION(std::string("CUDA error in file ") + __FILE__ + ":" + std::to_string(__LINE__) + ": " + cudaGetErrorString(err)); \
+    } \
+}
+
+#define CUDA_CHECK_ERROR() { \
+    cudaError_t err = cudaGetLastError(); \
+    if (cudaSuccess != err) { \
+      fprintf(stderr, "CUDA error in file '%s' in line %i: %s\n", \
+          __FILE__, __LINE__, cudaGetErrorString(err)); \
+      AIT_DEBUG_BREAK; \
+      AIT_EXCEPTION(std::string("CUDA error in file ") + __FILE__ + ":" + std::to_string(__LINE__) + ": " + cudaGetErrorString(err)); \
+    } \
+}
+
+namespace ait {
+
+class CudaManager {
+public:
+  static void deviceReset() {
+    CUDA_SAFE_CALL(cudaDeviceReset());
+  }
+
+  static int getDeviceCount() {
+    int device_count;
+    CUDA_SAFE_CALL(cudaGetDeviceCount(&device_count));
+    return device_count;
+  }
+
+  static int getDevice() {
+    int device;
+    CUDA_SAFE_CALL(cudaGetDevice(&device));
+    return device;
+  }
+
+  static void setDevice(const int device) {
+    CUDA_SAFE_CALL(cudaSetDevice(device));
+  }
+
+  static std::size_t getStackSize() {
+    std::size_t stack_size;
+    CUDA_SAFE_CALL(cudaDeviceGetLimit(&stack_size, cudaLimitStackSize));
+    return stack_size;
+  }
+
+  static void setStackSize(const std::size_t stack_size) {
+    CUDA_SAFE_CALL(cudaDeviceSetLimit(cudaLimitStackSize, stack_size));
+  }
+};
+
+class CudaUtils {
+public:
+  template <typename T>
+  static T* allocate() {
+    T* ptr;
+    CUDA_SAFE_CALL(cudaMalloc(&ptr, sizeof(T)));
+    return ptr;
+  }
+
+  template <typename T>
+  static T* allocate(const std::size_t count) {
+    T* ptr;
+    CUDA_SAFE_CALL(cudaMalloc(&ptr, count * sizeof(T)));
+    return ptr;
+  }
+
+  template <typename T>
+  static void deallocate(T* ptr) {
+    CUDA_SAFE_CALL(cudaFree(ptr));
+  }
+
+  template <typename T>
+  static void deallocate(T** ptr) {
+    CUDA_SAFE_CALL(cudaFree(*ptr));
+    *ptr = nullptr;
+  }
+
+  template <typename T>
+  static T copyFromDevice(const T* device_data) {
+    uint8_t host_data[sizeof(T)];
+    std::size_t size = sizeof(T);
+    CUDA_SAFE_CALL(cudaMemcpy(host_data, device_data, size, cudaMemcpyDeviceToHost));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+    return T(*reinterpret_cast<T*>(host_data));
+  }
+
+  template <typename T>
+  static void copyFromDevice(const T* device_data, T* host_data) {
+    std::size_t size = sizeof(T);
+    CUDA_SAFE_CALL(cudaMemcpy(host_data, device_data, size, cudaMemcpyDeviceToHost));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+  }
+
+  template <typename T>
+  static std::vector<T> copyArrayFromDevice(const T* device_data, std::size_t count) {
+    std::vector<T> host_data(count);
+    std::size_t size = sizeof(T) * count;
+    CUDA_SAFE_CALL(cudaMemcpy(host_data.data(), device_data, size, cudaMemcpyDeviceToHost));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+    return host_data;
+  }
+
+  template <typename T>
+  static void copyArrayFromDevice(const T* device_data, std::vector<T>* host_data) {
+    std::size_t size = sizeof(T) * host_data->size();
+    CUDA_SAFE_CALL(cudaMemcpy(host_data->data(), device_data, size, cudaMemcpyDeviceToHost));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+  }
+
+  template <typename T>
+  static void copyArrayFromDevice(const T* device_data, std::vector<T>* host_data, std::size_t count) {
+    host_data->resize(count);
+    std::size_t size = sizeof(T) * count;
+    CUDA_SAFE_CALL(cudaMemcpy(host_data->data(), device_data, size, cudaMemcpyDeviceToHost));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+  }
+
+  template <typename T>
+  static void copyToDevice(const T& host_data, T* device_data) {
+    std::size_t size = sizeof(T);
+    CUDA_SAFE_CALL(cudaMemcpy(device_data, &host_data, size, cudaMemcpyHostToDevice));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+  }
+
+  template <typename T>
+  static void copyArrayToDevice(const std::vector<T>& host_data, T* device_data) {
+    std::size_t size = sizeof(T) * host_data.size();
+    CUDA_SAFE_CALL(cudaMemcpy(device_data, host_data.data(), size, cudaMemcpyHostToDevice));
+
+#if !NO_CHECK_CUDA_CALLS
+    CUDA_DEVICE_SYNCHRONIZE();
+    CUDA_CHECK_ERROR();
+#endif
+  }
+};
+
+}
