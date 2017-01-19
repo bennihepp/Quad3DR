83d10691b153c84d08b576341a6ab79932e1b5ce
Benjamin Hepp <benjamin.hepp@posteo.de>
2017-01-19 13:51:36 +0100

    Added boost support header

---
+++ include/ait/boost.h
@@ -0,0 +1,13 @@
+//==================================================
+// boost.h
+//
+//  Copyright (c) 2017 Benjamin Hepp.
+//  Author: Benjamin Hepp
+//  Created on: Jan 18, 2017
+//==================================================
+
+#pragma once
+
+#ifndef BOOST_SYSTEM_NO_DEPRECATED
+#define BOOST_SYSTEM_NO_DEPRECATED 1
+#endif
